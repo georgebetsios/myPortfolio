35432505d4b80b47c011ddbb17874620d2ab3c60
George Betsios <betsiosgeorge@gmail.com>
2025-07-30 17:37:00 +0300

    Fix some texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className={`container ${fadeIn ? 'fade-in' : ''}`}>
         <h1 className="title">Hi, I'm George!</h1>
         <p className="intro">
-          I'm a Junior Full Stack Developer focused about building intuitive,
+          I'm a Junior Full Stack Developer focused on building intuitive,
           efficient, and scalable web applications from concept to deployment.
         </p>
 
