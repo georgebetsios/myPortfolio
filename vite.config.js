6d3aea4595618d9433b7a120ec9686b1c99828e6
George Betsios <betsiosgeorge@gmail.com>
2025-07-30 15:58:46 +0300

    Deploy with docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/myPortfolio/',
+  build: {
+    outDir: 'docs' // 🟡 αντί για dist
+  },
   plugins: [react()],
 });
 
